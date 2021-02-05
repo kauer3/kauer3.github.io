d3296a3bc270e86953e47974e6e3130e6f37166a
kauer3 <kaue0607@gmail.com>
2021-02-05 15:22:43 -0300

    Finished first section

--- scroll.js
+++ scroll.js
@@ -1,112 +1,56 @@
-//let tl = gsap.timeline({
-//scrollTrigger: {
-//trigger: '.dark',
-//start: "center bottom"
-//}
-//});
-
-//tl.from("img", {x: 200, opacity: 0, duration: 1.5})
-//tl.from("content", {y: 500, opacity: 0, duration: 1.5}, "-=1")
-
-//function swap() {
-//gsap.to(hello2, {
-//duration: 2,
-//opacity: 0,
-//scale: 0.8,
-//ease: "back",
-//delay: 2,
-//})
-//gsap.to(hello, {
-//duration: 2,
-//opacity: 0,
-//scale: 0.8,
-//ease: "back",
-//delay: 2,
-//})
-//}
-
 let tl = gsap.timeline()
 
-tl.from("img", {duration: 3, opacity: 0, x: 200, y: -200, scale: 0.6, ease: "elastic", delay: 1})
+tl.from("#dev", {duration: 30, opacity: 0, x: 200, y: -200, scale: 0.6, ease: "elastic", delay: 1})
 
-tl.from("#hello h1", {
+tl.from(".hello1", {
     duration: 1,
     opacity: 0,
     scale: 0.7,
     y: -300,
-    ease: "back",
-    //delay: 2,
-    stagger: 2
+    ease: "back"
+    // delay: 4,
+    // stagger: 3
+}, "-=26")
+
+gsap.to("#hello .hide", {
+    duration: 3,
+    opacity: 1,
+    scale: 1,
+    y: 250,
+    ease: "elastic",
+    delay: 10,
+    //markers: true,
+    stagger: 4.4
+    //onComplete: swap()
 })
 
-//tl.to(".hello1", {
-    //duration: 0.5,
-    //opacity: 0,
-    //scale: 0.7,
-    //y: 400,
-    ////ease: "expo",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=23")
+gsap.to("#hello .fade", {
+    duration: 0.5,
+    opacity: 0,
+    scale: 0.7,
+    y: 245,
+    ease: "expo",
+    delay: 10,
+    //markers: true,
+    stagger: 4.4
+    //onComplete: swap()
+})
 
-//tl.to(".hello2", {
-    //duration: 3,
-    //opacity: 1,
-    //scale: 1,
-    //y: 250,
-    //ease: "elastic",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=23")
+// tl.to("#dev", {
+//     duration: 5,
+//     x: -700,
+//     ease: "expo",
+//     onComplete: set,
+//     // delay: 25,
+//     // stagger: 3
+// }, "-=7")
 
-//tl.to(".hello2", {
-    //duration: 0.5,
-    //opacity: 0,
-    //scale: 0.7,
-    //y: 400,
-    ////ease: "expo",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=20")
-
-//tl.to(".hello3", {
-    //duration: 3,
-    //opacity: 1,
-    //scale: 1,
-    //y: 250,
-    //ease: "elastic",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=20")
-
-//tl.to(".hello3", {
-    //duration: 0.5,
-    //opacity: 0,
-    //scale: 0.7,
-    //y: 400,
-    ////ease: "expo",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=20")
-
-//tl.to(".hello4", {
-    //duration: 3,
-    //opacity: 1,
-    //scale: 1,
-    //y: 250,
-    //ease: "elastic",
-    ////delay: 2,
-    ////markers: true,
-    ////stagger: 2
-    ////onComplete: swap()
-//}, "-=18")
+// tl.from("#scroll", {
+//     duration: 3,
+//     opacity: 0,
+//     scale: 0.8,
+//     x: 400,
+//     ease: "bounce"
+//     // delay: 25,
+//     // stagger: 3
+// })
