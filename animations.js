5b2d830fb80ec08088a78cd1cb08fad87fdb4d0d
kauer3 <kaue0607@gmail.com>
2021-03-05 19:09:43 -0300

    Edited snaps for widescreen

--- animations.js
+++ animations.js
@@ -17,48 +17,175 @@ ScrollTrigger.create({
     // start: "top top",
     // end: "bottom bottom",
     pin: "#particles-js",
-    pinSpacing: true
+    pinSpacing: false
 })
 
-const projects = gsap.utils.toArray('.project');
+const containers = gsap.utils.toArray('.container');
+const titles = gsap.utils.toArray('.test');
+const contents1 = gsap.utils.toArray('.content1');
+const contents2 = gsap.utils.toArray('.content2');
+const contents3 = gsap.utils.toArray('.content3');
+const contents4 = gsap.utils.toArray('.content4');
+const contents5 = gsap.utils.toArray('.content5');
 var sections = gsap.utils.toArray('.snap-section');
 
 let n = 0;
-projects.forEach(project => {
+let i = 0;
+let contents = []
+containers.forEach(container => {
     n += 1;
+    switch (n) {
+
+        case 1:
+            contents = gsap.utils.toArray('.content1');
+            break;
+
+        case 2:
+            contents = gsap.utils.toArray('.content2');
+            break;
+
+        case 3:
+            contents = gsap.utils.toArray('.content3');
+            break;
+
+        case 4:
+            contents = gsap.utils.toArray('.content4');
+            break;
+
+        case 5:
+            contents = gsap.utils.toArray('.content5');
+            break;
+    }
     if (n % 2 === 0) {
-        origin = -800;
+        origin = -500;
     } else {
-        origin = 800;
+        origin = 500;
     };
-    gsap.from(project, {
-        x: origin,
+    gsap.from(container, {
+        // x: origin,
         opacity: 0,
         // yPercent: -1 / (6 - 1),
         // pin: true,
         scrollTrigger: {
-            start: "bottom bottom",
-            end: "top center",
-            trigger: project,
-            scrub: 0.5,
+            start: "center bottom",
+            end: "center center",
+            trigger: container,
+            scrub: 0.4,
             toggleActions: "play reverse play reverse",
             // duration: 0.5
             // markers: true,
             // pin: true
             // snap: {
-            //     snapTo: sections//".project", // snap to the closest label in the timeline
+            //     snapTo: sections//".container", // snap to the closest label in the timeline
             // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
             // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
             // ease: "none" // the ease of the snap animation ("power3" by default)
             // }
+            // duration: 2
+        }
+    })
+
+    contents.forEach(content => {
+        i += 1;
+        if (i % 2 === 0) {
+            origin = -1000;
+        } else {
+            origin = 1000;
+        };
+        gsap.from(content, {
+            x: origin,
+            opacity: 0,
+            // yPercent: -1 / (6 - 1),
+            // pin: true,
+            scrollTrigger: {
+                start: "top center",
+                end: "top center",
+                trigger: container,
+                scrub: 1,
+                toggleActions: "play reverse play reverse",
+                // duration: 0.5
+                // markers: true,
+                // pin: true
+                // snap: {
+                //     snapTo: sections//".content", // snap to the closest label in the timeline
+                // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+                // ease: "none" // the ease of the snap animation ("power3" by default)
+                // }
+                duration: 2
+            }
+        })
+    });
+});
+
+titles.forEach(title => {
+    n += 1;
+    if (n % 2 === 0) {
+        origin = -500;
+    } else {
+        origin = 500;
+    };
+    gsap.from(title, {
+        x: origin,
+        opacity: 0,
+        // yPercent: -1 / (6 - 1),
+        // pin: true,
+        scrollTrigger: {
+            start: "center center",
+            end: "top center",
+            trigger: title,
+            scrub: 1,
+            toggleActions: "play reverse play reverse",
+            // duration: 0.5
+            // markers: true,
+            // pin: true
+            // snap: {
+            //     snapTo: sections//".title", // snap to the closest label in the timeline
+            // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+            // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+            // ease: "none" // the ease of the snap animation ("power3" by default)
+            // }
+            duration: 2
         }
     })
 });
 
+// contents.forEach(content => {
+//     n += 1;
+//     if (n % 2 === 0) {
+//         origin = -1000;
+//     } else {
+//         origin = 1000;
+//     };
+//     gsap.from(content, {
+//         x: origin,
+//         opacity: 0,
+//         // yPercent: -1 / (6 - 1),
+//         // pin: true,
+//         scrollTrigger: {
+//             start: "center center",
+//             end: "top center",
+//             trigger: content,
+//             scrub: 2,
+//             toggleActions: "play reverse play reverse",
+//             // duration: 0.5
+//             // markers: true,
+//             // pin: true
+//             // snap: {
+//             //     snapTo: sections//".content", // snap to the closest label in the timeline
+//             // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+//             // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+//             // ease: "none" // the ease of the snap animation ("power3" by default)
+//             // }
+//             duration: 2
+//         }
+//     })
+// });
+
 ScrollTrigger.create({
     snap: {
         snapTo: 1 / 5,
-        // duration: 1.5
+        duration: 2
     }
 });
 
