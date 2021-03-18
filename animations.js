a982b0839ff660cd40fb1ea53840cb09ee486d15
kauer3 <kaue0607@gmail.com>
2021-03-18 02:44:10 -0300

    Adding smooth scroll

--- animations.js
+++ animations.js
@@ -4,18 +4,13 @@ particlesJS.load('particles-js', 'particles.json', function () {
     console.log('particles.json loaded!');
 });
 
-// Testing TsParticles instead of ParticlesJS
-// tsParticles.loadJSON('particles-js', 'particles.json');
 
-// Cursor
+// GSAP
 
-gsap.registerPlugin(ScrollTrigger);
-// gsap.to(sections, {
-//     yPercent: -100 * (sections.length - 1),
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
 ScrollTrigger.create({
     trigger: "body",
-    // start: "top top",
-    // end: "bottom bottom",
     pin: "#particles-js",
     pinSpacing: false
 })
@@ -28,6 +23,16 @@ const contents3 = gsap.utils.toArray('.content3');
 const contents4 = gsap.utils.toArray('.content4');
 const contents5 = gsap.utils.toArray('.content5');
 var sections = gsap.utils.toArray('.snap-section');
+// const project5 = document.querySelector("#project5");
+
+// TODO Finished implementing this
+document.querySelector("#smooth").addEventListener("click", function () {
+    e.preventDefault();
+    // gsap.to(window, {duration: 2, scrollTo: "#projects"});
+    // const section = document.querySelector(link.getAttribute("href"));
+    // return window.scrollTo("#projects"));
+    gsap.to(sections, 2, {scrollTo: "#smooth"});
+});
 
 let n = 0;
 let i = 0;
@@ -63,31 +68,16 @@ containers.forEach(container => {
         origin = 500;
     };
     gsap.from(container, {
-        // x: origin,
         css: {
             height: 0,
-            // width: "-=250",
-            // backgroundColor: "transparent"
             opacity: 0,
         },
-        // yPercent: -1 / (6 - 1),
-        // pin: true,
         scrollTrigger: {
             start: "bottom bottom",
             end: "center center",
             trigger: container,
             scrub: 0.5,
             toggleActions: "play reverse play reverse",
-            // duration: 0.5
-            // markers: true,
-            // pin: true
-            // snap: {
-            //     snapTo: sections//".container", // snap to the closest label in the timeline
-            // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-            // ease: "none" // the ease of the snap animation ("power3" by default)
-            // }
-            // duration: 2
         }
     })
 
@@ -101,24 +91,12 @@ containers.forEach(container => {
         gsap.from(content, {
             x: origin,
             opacity: 0,
-            // width: 9000, was causing content to not resize responsivelly
-            // yPercent: -1 / (6 - 1),
-            // pin: true,
             scrollTrigger: {
                 start: "top center",
                 end: "top center",
                 trigger: container,
                 scrub: 2,
                 toggleActions: "play reverse play reverse",
-                // duration: 0.5
-                // markers: true,
-                // pin: true
-                // snap: {
-                //     snapTo: sections//".content", // snap to the closest label in the timeline
-                // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-                // ease: "none" // the ease of the snap animation ("power3" by default)
-                // }
                 duration: 2
             }
         })
@@ -135,63 +113,16 @@ titles.forEach(title => {
     gsap.from(title, {
         x: origin,
         opacity: 0,
-        // css: {
-        //     fontSize: "10px",
-        //     // backgroundColor: "transparent"
-        // },
-        // yPercent: -1 / (6 - 1),
-        // pin: true,
         scrollTrigger: {
             start: "50%-=800px",
             end: "top center",
             trigger: title,
             scrub: 0.1,
             toggleActions: "play reverse play reverse",
-            // duration: 0.5
-            // markers: true,
-            // pin: true
-            // snap: {
-            //     snapTo: sections//".title", // snap to the closest label in the timeline
-            // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-            // ease: "none" // the ease of the snap animation ("power3" by default)
-            // }
             duration: 2
         }
     })
 });
-
-// contents.forEach(content => {
-//     n += 1;
-//     if (n % 2 === 0) {
-//         origin = -1000;
-//     } else {
-//         origin = 1000;
-//     };
-//     gsap.from(content, {
-//         x: origin,
-//         opacity: 0,
-//         // yPercent: -1 / (6 - 1),
-//         // pin: true,
-//         scrollTrigger: {
-//             start: "center center",
-//             end: "top center",
-//             trigger: content,
-//             scrub: 2,
-//             toggleActions: "play reverse play reverse",
-//             // duration: 0.5
-//             // markers: true,
-//             // pin: true
-//             // snap: {
-//             //     snapTo: sections//".content", // snap to the closest label in the timeline
-//             // duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-//             // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-//             // ease: "none" // the ease of the snap animation ("power3" by default)
-//             // }
-//             duration: 2
-//         }
-//     })
-// });
 
 ScrollTrigger.create({
     snap: {
@@ -200,32 +131,3 @@ ScrollTrigger.create({
         ease: "sine"
     }
 });
-
-// gsap.to("#first-project", {
-//     ScrollTrigger: {
-//         trigger: "#first-project",
-//         start: "center top",
-//         end: "bottom top",
-//         scrub: 1,
-//         pin: true,
-//         markers: true
-//     },
-//     x: -500,
-//     rotation: 180,
-//     ease: "none",
-//     duration: 3,
-//     toggleActions: "play none reverse none"
-// });
-
-// const cursor = document.querySelector('.cursor');
-// document.addEventListener('mousemove', e => {
-//     cursor.setAttribute('style', 'top: ' + (e.pageY - 5) + 'px; left:' + (e.pageX - 4.1) + 'px;')
-// })
-
-// document.addEventListener('click', () => {
-//     cursor.classList.add("expand");
-
-//     setTimeout(() => {
-//         cursor.classList.remove("expand");
-//     }, 500)
-// })
